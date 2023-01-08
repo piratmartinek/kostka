501676df9876b96cb69d848a1109269e68d23322
piratmartinek <121960967+piratmartinek@users.noreply.github.com>
2023-01-08 11:34:10 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("ahoj")
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(2000)
+})
 input.onGesture(Gesture.ScreenUp, function () {
     if (number > 0) {
         basic.showNumber(number)
@@ -7,6 +28,47 @@ input.onGesture(Gesture.ScreenUp, function () {
 })
 input.onGesture(Gesture.ScreenDown, function () {
     basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("ahoj")
+    basic.showIcon(IconNames.LeftTriangle)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("ahoj")
+    basic.showIcon(IconNames.Triangle)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(2000)
 })
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 4; index++) {
